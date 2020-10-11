ee007baadb5905ccb624bec17f157c1a977fc163
heildever <heildever@protonmail.com>
2020-10-11 13:36:02 -0400

    shuffle-the-array

---
+++ python/shuffle-the-array.py
@@ -0,0 +1,11 @@
+# question can be found at leetcode.com/problems/shuffle-the-array/
+from Typing import List
+
+
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        rl = []
+        for idx, num in enumerate(nums[:n]):
+            rl.append(num)
+            rl.append(nums[idx + n])
+        return rl
