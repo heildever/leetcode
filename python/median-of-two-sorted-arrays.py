53c62ab4c1955eea52df9c0e3643ce6fb8780f91
heildever <heildever@protonmail.com>
2020-10-13 14:42:49 -0400

    median-of-two-sorted-arrays

---
+++ python/median-of-two-sorted-arrays.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int],
+                               nums2: List[int]) -> float:
+        nums = sorted(nums1 + nums2)
+        len_nums = len(nums)
+        median_idx = len_nums // 2
+        if len_nums % 2 == 0:
+            return nums[median_idx - 1] + nums[median_idx] / 2
+        else:
+            return nums[median_idx]
