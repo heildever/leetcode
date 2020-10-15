2e61e8e3569d026a0676b3173dd1d30827929688
heildever <heildever@protonmail.com>
2020-10-15 11:17:38 -0400

    fixing set intersection

--- python/intersection-of-two-arrays.py
+++ python/intersection-of-two-arrays.py
@@ -4,4 +4,4 @@ from Typing import List
 
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        return set(nums2).intersection(set(nums2))
+        return set(nums1).intersection(set(nums2))
