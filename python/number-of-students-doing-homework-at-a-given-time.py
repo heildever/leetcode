8e93ecb564ec190806bc950570e30c2cb094a3c3
heildever <heildever@protonmail.com>
2020-10-21 21:17:16 -0400

    number-of-students-doing-homework

---
+++ python/number-of-students-doing-homework-at-a-given-time.py
@@ -0,0 +1,11 @@
+# question can be found on leetcode.com/problems/number-of-students-doing-homework-at-a-given-time/
+from Typing import List
+
+
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int],
+                    queryTime: int) -> int:
+        return len([
+            1 for st_i, et_i in zip(startTime, endTime)
+            if (et_i >= queryTime >= st_i)
+        ])
