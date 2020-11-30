80d85158d6f2529d120390dd030074e194550d97
heildever <heildever@protonmail.com>
2020-11-29 22:40:38 -0500

    relative-ranks

---
+++ python/relative-ranks.py
@@ -0,0 +1,14 @@
+# question can be found on leetcode.com/problems/relative-ranks/
+from typing import List
+
+
+class Solution:
+    def findRelativeRanks(self, nums: List[int]) -> List[str]:
+        lookup = {
+            score: rank
+            for rank, score in enumerate(sorted(nums, reverse=True))
+        }
+        medals = ["Gold Medal", "Silver Medal", "Bronze Medal"
+                  ] + [str(i) for i in range(4,
+                                             len(nums) + 1)]
+        return [medals[lookup[n]] for n in nums]
