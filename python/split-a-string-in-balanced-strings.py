f72a4eefa4ae9c0fe220e9b793197216b353188e
heildever <heildever@protonmail.com>
2020-12-02 22:36:00 -0500

    split-a-string-in-balanced-strings

---
+++ python/split-a-string-in-balanced-strings.py
@@ -0,0 +1,25 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        # my way
+        stack = [0]
+        for char in s:
+            if char == "R":
+                stack.append(stack[-1] + 1)
+            elif char == "L":
+                stack.append(stack[-1] - 1)
+        return stack.count(0) - 1
+
+        # one could rely on only "R" and "L" will be in s
+        # this would have less space complexity
+        result = counter = 0
+        # here counter keeps the tab for "RL" matches
+        for char in s:
+            if char == "R":
+                counter += 1
+            else:
+                counter -= 1
+
+            if counter == 0:
+                result += 1
+
+        return result
