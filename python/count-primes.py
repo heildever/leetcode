fe8111b4976a5b30844878fe13edd07a0aa88f9d
heildever <heildever@protonmail.com>
2020-12-12 18:59:48 -0500

    reformat

--- python/count-primes.py
+++ python/count-primes.py
@@ -6,9 +6,9 @@ class Solution:
         holder = [1] * n
         holder[0] = holder[1] = 0
 
-        for number in range(2, int(n**0.5)+1):
+        for number in range(2, int(n**0.5) + 1):
             if holder[number] == 1:
                 for n in range(number**2, n, number):
                     holder[n] = 0
-        
+
         return sum(holder)
