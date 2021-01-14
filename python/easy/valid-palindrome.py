e58f0ae9225886958ea2a88f2e0289023cd483d7
anidever <anidever@outlook.com>
2021-01-13 22:03:37 -0500

    Add question url

--- python/easy/valid-palindrome.py
+++ python/easy/valid-palindrome.py
@@ -1,3 +1,6 @@
+# question can be found on leetcode.com/problems/valid-palindrome
+
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         clean_s = "".join([c for c in s if c.isalnum()]).upper()
